81aeab62c6255f5e27dcfff4f017f1e6978b465b
Aliw7979 <ali.black.viper@gmail.com>
2023-05-26 14:26:20 +0330

    acc finished

--- app/src/main/java/com/example/singleplayerponggame/BallView.java
+++ app/src/main/java/com/example/singleplayerponggame/BallView.java
@@ -4,20 +4,20 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.AttributeSet;
 import android.view.View;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorListener;
-import android.hardware.SensorManager;
 
-public class BallView extends View {
+public class BallView extends View implements SensorEventListener {
 
-    private SensorManager sensorManager;
-    private Sensor acceleromoeter;
-    private Sensor rotationSensor;
-    private Sensor gyro;
+    private static final float ALPHA =1f;
+    private float[] gravity = new float[3];
+    private float[] linearAcceleration = new float[3];
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
     private Paint paint = new Paint();
     private float x = -10f; // X coordinate of the ball
     private float y = -10f; // Y coordinate of the ball
@@ -28,8 +28,13 @@ public class BallView extends View {
     private float discWidth = -10f;
     private float discHeight = 25f;
 
+    private float dxDisc = 0f;
+    private float dyDisc = 0f;
+
     public BallView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mSensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
     }
 
     @Override
@@ -47,6 +52,7 @@ public class BallView extends View {
             discWidth = getWidth() / 3f;
         }
 
+
         // Set the background color to white
         canvas.drawColor(Color.WHITE);
 
@@ -56,6 +62,15 @@ public class BallView extends View {
         // Update the ball position
         x += dx;
         y += dy;
+
+        if(discX + dxDisc > getWidth() - 1 || discX + dxDisc < -discWidth)
+        {
+            discX += 0;
+        }
+        else {
+            discX += dxDisc;
+        }
+        discY += dyDisc;
 
         // Check if the ball hits the edges of the view
         if (x > getWidth() - 25 || x < 25) {
@@ -69,7 +84,62 @@ public class BallView extends View {
         canvas.drawCircle(x, y, 25f, paint);
         canvas.drawRect(discX, discY, discX + discWidth, discHeight + discY, paint);
 
+
         // Redraw the view on the next frame
         invalidate();
     }
-}
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        mSensorManager.unregisterListener(this);
+    }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+
+            if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+                // Apply high-pass filter to remove gravity component
+                gravity[0] = ALPHA * gravity[0] + (1 - ALPHA) * event.values[0];
+                gravity[1] = ALPHA * gravity[1] + (1 - ALPHA) * event.values[1];
+                gravity[2] = ALPHA * gravity[2] + (1 - ALPHA) * event.values[2];
+
+                linearAcceleration[0] = event.values[0] - gravity[0];
+                linearAcceleration[1] = event.values[1] - gravity[1];
+                linearAcceleration[2] = event.values[2] - gravity[2];
+
+                // Use the filtered linear acceleration values to move the ball
+                // For example:
+                float xAcceleration = linearAcceleration[0];
+//                float yAcceleration = linearAcceleration[1];
+                if(-xAcceleration > 0 && -xAcceleration > 2)
+                {
+                    dxDisc = 35;
+                }
+                else if (-xAcceleration < 0 && -xAcceleration < -2){
+                    dxDisc = -35;
+                }
+                else {
+                    dxDisc = 0;
+                }
+            }
+
+
+            // Adjust the ball position based on the accelerometer readings
+            /*dyDisc = yAcceleration * 50f;*/
+        }
+/*dyDisc = yAcceleration * 50f;*/
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // Not used
+        }
+    }
+
+
+
--- app/src/main/java/com/example/singleplayerponggame/MainActivity.java
+++ app/src/main/java/com/example/singleplayerponggame/MainActivity.java
@@ -1,19 +1,22 @@
 package com.example.singleplayerponggame;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import androidx.activity.ComponentActivity;
-import androidx.compose.material3.MaterialTheme;
-import androidx.compose.runtime.Composable;
-import androidx.compose.ui.Modifier;
-import androidx.compose.ui.tooling.preview.Preview;
+
 
 public class MainActivity extends ComponentActivity {
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Set the content view to the main layout
         setContentView(R.layout.activity_main);
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         // Get a reference to the ball view
         View ballView = findViewById(R.id.ball_view);
